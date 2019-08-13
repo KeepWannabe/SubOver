726ea762b9e6bf848624a5f6a6d1764fdec1a161
Abdi Prawira N <wearemasskill@outlook.com>
2019-08-13 07:21:49 -0700

    Update subover.go

--- subover.go
+++ subover.go
@@ -39,7 +39,7 @@ var (
 )
 
 func InitializeProviders() {
-    raw, err := ioutil.ReadFile("$HOME/.0verscan/providers.json")
+    raw, err := ioutil.ReadFile("~/.0verscan/providers.json")
     if err != nil {
         fmt.Println(err.Error())
         os.Exit(1)
